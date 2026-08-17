ef06be31387bbce2aeaf995c8d6515448f2df002
Umang Agarwal <umang01aga@gmail.com>
2026-08-17 10:04:12 +0530

    Update users.py

--- app/users.py
+++ app/users.py
@@ -10,8 +10,9 @@ from fastapi_users.authentication import(
 
 from fastapi_users.db import SQLAlchemyUserDatabase
 from app.db import User , get_user_db
+import os
 
-SECRET = "umang01aga"
+SECRET = os.getenv("SECRET")
 
 class UserManager(UUIDIDMixin,BaseUserManager[User,uuid.UUID]):
     reset_password_token_secret=SECRET
